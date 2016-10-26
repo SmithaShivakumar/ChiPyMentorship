ecad8b2deb81754ca0fa1604635faea98a7dfb47
SmithaShivakumar <smithashivakumar@rocketmail.com>
2016-10-26 09:24:52 -0500

    updated to get the right output

--- excelToCsv.py
+++ excelToCsv.py
@@ -6,16 +6,19 @@ from os import sys
 def csv_from_excel(excel_file):
     workbook = xlrd.open_workbook(excel_file)
     all_worksheets = workbook.sheet_names()
+
     for worksheet_name in all_worksheets:
         worksheet = workbook.sheet_by_name(worksheet_name)
-        your_csv_file = open(''.join([worksheet_name,'.csv']), 'w')
+        your_csv_file = open(''.join([worksheet_name,'.csv']), 'w', newline = '')
         wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
 
         for rownum in range(worksheet.nrows):
-            for col_num in range ( worksheet.ncols ) :
-                value = str ( worksheet.row (rownum ) [ col_num ].value )
-                wr.writerow(list(x.encode('utf-8') if type(x) == type(u'') else x for x in worksheet.row_values(rownum)))
+            # for col_num in range ( worksheet.ncols ) :
+            value = str ( worksheet.row (rownum)[1].value )
+            # non_blank = (line for line in value if line.strip())
+            # print(non_blank)
+            wr.writerow(x for x in worksheet.row_values(rownum))#list(x.encode('utf-8') if type(x) == type(u'') else x for x in worksheet.row_values(rownum)))
         your_csv_file.close()
 
 #if __name__ == "__main__":
-csv_from_excel('G:/Metrixx/Data/ETF_MostTraded.xlsx')
+csv_from_excel('C:/Users/Tenzan/Documents/Smitha/straddle_test/ForRepo/Smitha/Data_Universe.xlsx')
